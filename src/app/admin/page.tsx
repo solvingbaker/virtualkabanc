4089f1c43195e9b81b3cc7fc180b77b9e948076f
solvingbaker <hollow_alexis@hotmail.com>
2024-01-20 00:50:35 -0700

    CHANGES

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,30 +1,37 @@
 "use client";
+import React from "react";
 import HighchartsChart from "./components/HighchartsChart";
 
-function Page(username: string) {
-  
-        return (
-            <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-                <main className="flex flex-col  items-center justify-center w-full flex-1 px-20 text-center">
-                    <div className="bg-white rounded-2xl shadow-xl flex w-2/3 max-w-full">
 
-                        <div className="w-11/12 p-5">
-                            <div className="text-left font-bold">
-                                <span className="text-violet-500 ">Bienvenido </span> <span className="text-black">{username}</span>
-                            </div>
-                            <div className="pt-2">
-                                <h2 className="text-base font-bold text-left text-violet-500 mb-2 yr-1/2">Tu estado de cuenta</h2>
-                            </div>
-                            <div>
-                                 <HighchartsChart/>
-                            </div>
+interface AdminInterface {
+    username: string;
+}
+
+const Page: React.FC<AdminInterface> = (props) => {
+    const { username } = props;
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+            <main className="flex flex-col  items-center justify-center w-full flex-1 px-20 text-center">
+                <div className="bg-white rounded-2xl shadow-xl flex w-2/3 max-w-full">
+
+                    <div className="w-11/12 p-5">
+                        <div className="text-left font-bold">
+                            <span className="text-violet-500 ">Bienvenido </span> <span className="text-black">{username}</span>
+                        </div>
+                        <div className="pt-2">
+                            <h2 className="text-base font-bold text-left text-violet-500 mb-2 yr-1/2">Tu estado de cuenta</h2>
+                        </div>
+                        <div>
+                            <HighchartsChart />
                         </div>
                     </div>
+                </div>
 
 
-                </main>
-            </div>
-        );
-    }
+            </main>
+        </div>
+    );
+}
 
-    export default Page;
+export default Page;
